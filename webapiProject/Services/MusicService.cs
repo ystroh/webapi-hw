70039f855abe2959f2870971d23e7437f8977ab3
הנדסת תוכנה <huser@sem.local>
2025-12-10 15:20:50 +0200

    write to json file

--- webapiProject/Services/MusicService.cs
+++ webapiProject/Services/MusicService.cs
@@ -2,25 +2,53 @@ using Microsoft.AspNetCore.Mvc;
 using myMusic.Models;
 using System.Security.Cryptography.X509Certificates;
 using MusicService.interfaces;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
 namespace myMusic.Services;
 
 public class MusicService : IMusicServices
 {
   
 
-     private List<Music> list;
+    private List<Music> list;
+     private IWebHostEnvironment  webHost;
+     private string filePath ;
 
-    public MusicService()
+    public MusicService(IWebHostEnvironment webHost)
     {
-        this.list = new List<Music>{
-             new Music { Id = 1, Name = "guittar",IsWoodMade=true},
-             new Music { Id = 2, Name = "fiddle",IsWoodMade=true},
-             new Music { Id = 3, Name = "organ",IsWoodMade=true},
-             new Music { Id = 4, Name = "piano",IsWoodMade=false} 
-        };
+        this.webHost = webHost;
+        // this.list = new List<Music>{
+        //      new Music { Id = 1, Name = "guittar",IsWoodMade=true},
+        //      new Music { Id = 2, Name = "fiddle",IsWoodMade=true},
+        //      new Music { Id = 3, Name = "organ",IsWoodMade=true},
+        //      new Music { Id = 4, Name = "piano",IsWoodMade=false} 
+        // };
+
+
+           this.filePath = Path.Combine(webHost.ContentRootPath, "Data", "music.json");
+            using (var jsonFile = File.OpenText(filePath))
+            {
+                var content = jsonFile.ReadToEnd();
+                list = JsonSerializer.Deserialize<List<Music>>(content,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
     }
    
    
+        private void saveToFile()
+        {
+            var text = JsonSerializer.Serialize(list);
+            File.WriteAllText(filePath, text);
+        }
 
     public List<Music> Get()
     {
@@ -40,7 +68,9 @@ public class MusicService : IMusicServices
         var maxId = list.Max(p => p.Id);
         newMusic.Id = maxId + 1;
         list.Add(newMusic);
+         saveToFile();
             return newMusic;
+        
     }
 
     public int Update(int id, Music newMusic)
@@ -54,7 +84,7 @@ public class MusicService : IMusicServices
 
         var index = list.IndexOf(m);
         list[index] = newMusic;
-
+        saveToFile();
         return 3;
     }
 
@@ -65,6 +95,9 @@ public class MusicService : IMusicServices
         if(m==null)
             return false;
         list.Remove(m);
+       
+
+          saveToFile();
         return true;
     }
 }
--- webapiProject/Services/UsersService.cs
+++ webapiProject/Services/UsersService.cs
@@ -2,6 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 using myUsers.Models;
 using System.Security.Cryptography.X509Certificates;
 using UsersService.interfaces;
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
 namespace myUsers.Services;
 
 public class UsersService : IUsersServices
@@ -9,18 +18,38 @@ public class UsersService : IUsersServices
   
 
      private List<Users> list;
-
-    public UsersService()
+ private IWebHostEnvironment  webHost;
+     private string filePath ;
+    public UsersService(IWebHostEnvironment webHost)
     {
-        this.list = new List<Users>{
-             new Users { Id = 1, Name = "yehudit",password=1},
-             new Users { Id = 2, Name = "chaya",password=2},
-             new Users { Id = 3, Name = "elisheva",password=3},
-             new Users { Id = 4, Name = "nomi",password=4} 
-        };
+        // this.list = new List<Users>{
+        //      new Users { Id = 1, Name = "yehudit",password=1},
+        //      new Users { Id = 2, Name = "chaya",password=2},
+        //      new Users { Id = 3, Name = "elisheva",password=3},
+        //      new Users { Id = 4, Name = "nomi",password=4} 
+        // };
+
+         this.webHost = webHost;
+      
+
+
+           this.filePath = Path.Combine(webHost.ContentRootPath, "Data", "users.json");
+            using (var jsonFile = File.OpenText(filePath))
+            {
+                var content = jsonFile.ReadToEnd();
+                list = JsonSerializer.Deserialize<List<Users>>(content,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
     }
    
-   
+    private void saveToFile()
+        {
+            var text = JsonSerializer.Serialize(list);
+            File.WriteAllText(filePath, text);
+        }
 
     public List<Users> Get()
     {
@@ -40,6 +69,7 @@ public class UsersService : IUsersServices
         var maxId = list.Max(p => p.Id);
         newUsers.Id = maxId + 1;
         list.Add(newUsers);
+        saveToFile();
             return newUsers;
     }
 
@@ -54,7 +84,7 @@ public class UsersService : IUsersServices
 
         var index = list.IndexOf(m);
         list[index] = newUsers;
-
+saveToFile();
         return 3;
     }
 
@@ -65,6 +95,7 @@ public class UsersService : IUsersServices
         if(m==null)
             return false;
         list.Remove(m);
+        saveToFile();
         return true;
     }
 }
